6a1a1665f7c952d4913095c128dd4b38dc85b5dd
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2014-12-27 11:52:55 +0200

    raities clean up

--- lib/mv/sqlite/railtie.rb
+++ lib/mv/sqlite/railtie.rb
@@ -3,9 +3,14 @@ require 'mv/sqlite/validation/base_decorator'
 module Mv
   module Sqlite
     class Railtie < ::Rails::Railtie
+      initializer 'mv-sqlite.initialization' do
+        ActiveSupport.on_load(:active_record) do
+          ::ActiveRecord::ConnectionAdapters::SQLite3Adapter.send(:prepend, Mv::Core::ActiveRecord::ConnectionAdapters::AbstractAdapterDecorator)
+        end
 
-      initializer 'mv-sqlite.initialization', after: 'mv-core.initialization.active_record' do
-        Mv::Core::Validation::Base.send(:prepend, Mv::Sqlite::Validation::BaseDecorator)
+        ActiveSupport.on_load(:mv_core) do
+          Mv::Core::Validation::Base.send(:prepend, Mv::Sqlite::Validation::BaseDecorator)
+        end
       end
     end
   end
