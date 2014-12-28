b1b46c1a856d305856c702574ae5a18f3217f595
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2014-12-28 22:56:35 +0200

    update validation integration test

---
+++ spec/mv/sqlite/integration/update_validations_spec.rb
@@ -0,0 +1,80 @@
+require 'spec_helper'
+
+describe 'Update validation scenarios' do
+  before do
+    Mv::Core::Services::CreateMigrationValidatorsTable.new.execute
+    ::ActiveRecord::ConnectionAdapters::SQLite3Adapter.send(:prepend, Mv::Sqlite::ActiveRecord::ConnectionAdapters::Sqlite3AdapterDecorator)
+  end
+
+  describe 'update column in change_table block' do
+    before do
+      Class.new(::ActiveRecord::Migration) do
+        def change
+          create_table :table_name, id: false do |t|
+            t.string :column_name, validates: { uniqueness: { as: :trigger, on: :create } }
+          end
+        end
+      end.new('TestMigration', '20141118164617').migrate(:up)
+    end
+
+    subject do
+       Class.new(::ActiveRecord::Migration) do
+        def change
+          change_table :table_name, id: false do |t|
+            t.change :column_name, :string, validates: { uniqueness: { as: :index } }
+          end
+        end
+      end.new('TestMigration', '20141118164617').migrate(:up)
+    end
+
+    it "deletes trigger constraint" do
+      expect_any_instance_of(Mv::Core::Constraint::Trigger).to receive(:delete).once
+      subject
+    end
+
+    it "creates index constraint" do
+      expect_any_instance_of(Mv::Core::Constraint::Index).to receive(:create).once
+      subject
+    end
+
+    it "updates migration validator" do
+      expect{ subject }.to change{Mv::Core::Db::MigrationValidator.first.options}.from(as: :trigger, on: :create) 
+                                                                                  .to(as: :index)
+    end
+  end
+
+  describe 'standalone update column statement' do
+    before do
+      Class.new(::ActiveRecord::Migration) do
+        def change
+          create_table :table_name, id: false do |t|
+            t.string :column_name, validates: { uniqueness: { as: :trigger, on: :create } }
+          end
+        end
+      end.new('TestMigration', '20141118164617').migrate(:up)
+    end
+
+    subject do
+       Class.new(::ActiveRecord::Migration) do
+        def change
+          change_column :table_name, :column_name, :string, validates: { uniqueness: { as: :index } }
+        end
+      end.new('TestMigration', '20141118164617').migrate(:up)
+    end
+
+    it "deletes trigger constraint" do
+      expect_any_instance_of(Mv::Core::Constraint::Trigger).to receive(:delete).once
+      subject
+    end
+
+    it "creates index constraint" do
+      expect_any_instance_of(Mv::Core::Constraint::Index).to receive(:create).once
+      subject
+    end
+
+    it "updates migration validator" do
+      expect{ subject }.to change{Mv::Core::Db::MigrationValidator.first.options}.from(as: :trigger, on: :create) 
+                                                                                  .to(as: :index)
+    end
+  end
+end
