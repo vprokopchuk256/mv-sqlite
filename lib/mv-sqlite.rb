37a20ff0a78f054751dc171bb198914743cd6385
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2014-12-28 21:03:23 +0200

    raitie cleanup

--- lib/mv-sqlite.rb
+++ lib/mv-sqlite.rb
@@ -1,5 +1,10 @@
 require 'mv-core'
+require 'mv/sqlite/validation/base_decorator'
 require 'mv/sqlite/railtie'
+
+ActiveSupport.on_load(:mv_core) do
+  Mv::Core::Validation::Base.send(:prepend, Mv::Sqlite::Validation::BaseDecorator)
+end
 
 
 
--- lib/mv/sqlite/railtie.rb
+++ lib/mv/sqlite/railtie.rb
@@ -4,14 +4,8 @@ require 'mv/sqlite/active_record/connection_adapters/sqlite3_adapter_decorator'
 module Mv
   module Sqlite
     class Railtie < ::Rails::Railtie
-      initializer 'mv-sqlite.initialization' do
-        ActiveSupport.on_load(:active_record) do
-          ::ActiveRecord::ConnectionAdapters::SQLite3Adapter.send(:prepend, described_class)
-        end
-
-        ActiveSupport.on_load(:mv_core) do
-          Mv::Core::Validation::Base.send(:prepend, Mv::Sqlite::Validation::BaseDecorator)
-        end
+      initializer 'mv-sqlite.initialization', after: 'active_record.initialize_database' do
+        ::ActiveRecord::ConnectionAdapters::SQLite3Adapter.send(:prepend, Mv::Sqlite::ActiveRecord::ConnectionAdapters::Sqlite3AdapterDecorator)
       end
     end
   end
